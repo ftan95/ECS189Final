8afb9693df8ffebd9da628df018b56639275df7f
Frederick <Frederick@Pein-2.local>
2019-12-01 15:19:27 -0800

    Kind of added a portal

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void LoadNextLevel(int x)
+    {
+        SceneManager.LoadScene(x);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour
+{
+    private GameManager Gm;
+    public int Next;
+    public bool test = false;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (this.test == true)
+        {
+            this.Gm.LoadNextLevel(Next);
+        }
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Gm = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
