4d34826e6a54657b5e660735ae45141b437dcf43
gbrusman <gabrielbrusman@gmail.com>
2019-12-03 18:50:45 -0800

    Added ADSR curve to player movement. Also started trying to implement a downwards raycast so that we know whether or not we can jump (i.e. so we know whether we are on the ground or not), but the raycast isn't working yet. I also changed the input for moving left and right to A and D because I thought WASD might be more intuitive for gamers, but we can change it to whatever we want.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,59 +4,165 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float Speed;
+    private float DistanceToGround = 1.285f;
+    //bool CanJump = true;
+    //bool CanWalk = true;
     // Start is called before the first frame update
+
+    [SerializeField] private float Speed = 10.0f;
+
+    [SerializeField] private float AttackDuration = 0.5f;
+    [SerializeField] private AnimationCurve Attack;
+
+    [SerializeField] private float DecayDuration = 0.25f;
+    [SerializeField] private AnimationCurve Decay;
+
+    [SerializeField] private float SustainDuration = 5.0f;
+    [SerializeField] private AnimationCurve Sustain;
+
+    [SerializeField] private float ReleaseDuration = 0.25f;
+    [SerializeField] private AnimationCurve Release;
+
+    private float AttackTimer;
+    private float DecayTimer;
+    private float SustainTimer;
+    private float ReleaseTimer;
+
+    private float InputDirection = 0.0f;
+
+    private enum Phase { Attack, Decay, Sustain, Release, None};
+
+    private Phase CurrentPhase;
+
     void Start()
     {
-        
+        this.CurrentPhase = Phase.None;
+    }
+
+
+    float ADSREnvelope()
+    {
+        float velocity = 0.0f;
+
+        if(Phase.Attack == this.CurrentPhase)
+        {
+            velocity = this.Attack.Evaluate(this.AttackTimer / this.AttackDuration);
+            this.AttackTimer += Time.deltaTime;
+            if(this.AttackTimer > this.AttackDuration)
+            {
+                this.CurrentPhase = Phase.Decay;
+            }
+        } 
+        else if(Phase.Decay == this.CurrentPhase)
+        {
+            velocity = this.Decay.Evaluate(this.DecayTimer / this.DecayDuration);
+            this.DecayTimer += Time.deltaTime;
+            if (this.DecayTimer > this.DecayDuration)
+            {
+                this.CurrentPhase = Phase.Sustain;
+            }
+        } 
+        else if (Phase.Sustain == this.CurrentPhase)
+        {
+            velocity = this.Sustain.Evaluate(this.SustainTimer / this.SustainDuration);
+            this.SustainTimer += Time.deltaTime;
+            if (this.SustainTimer > this.SustainDuration)
+            {
+                this.CurrentPhase = Phase.Release;
+            }
+        } 
+        else if (Phase.Release == this.CurrentPhase)
+        {
+            velocity = this.Release.Evaluate(this.ReleaseTimer / this.ReleaseDuration);
+            this.ReleaseTimer += Time.deltaTime;
+            if (this.ReleaseTimer > this.ReleaseDuration)
+            {
+                this.CurrentPhase = Phase.None;
+            }
+        }
+        return velocity;
+    }
+
+    private void ResetTimers()
+    {
+        this.AttackTimer = 0.0f;
+        this.DecayTimer = 0.0f;
+        this.SustainTimer = 0.0f;
+        this.ReleaseTimer = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //float horizontal = Input.GetAxisRaw("Horizontal");
-        //Vector3 direction = new Vector3(horizontal, 0, 0);
-        //gameObject.transform.Translate(direction.normalized * Time.deltaTime * this.Speed);
 
         var rigidBody = gameObject.GetComponent<Rigidbody2D>();
 
-        if (Input.GetAxis("Horizontal") > 0.01)
+        //Debug.Log(Input.GetAxis("Horizontal"));
+        if (Input.GetButtonDown("D")) 
         {
-            ExecuteRight(this.gameObject);
-        }
-        if (Input.GetAxis("Horizontal") < -0.01)
-        {
-            ExecuteLeft(this.gameObject);
+            Debug.Log("Pressing Right");
+            this.ResetTimers();
+            this.CurrentPhase = Phase.Attack;
+            this.InputDirection = 1.0f;
         }
 
+        if (Input.GetButton("D"))  
+        {
+            this.InputDirection = 1.0f;
+        }
 
-        if (Input.GetAxis("Vertical") > 0.01)
+        if (Input.GetButtonUp("D"))
+        {
+            this.InputDirection = 1.0f;
+            this.CurrentPhase = Phase.Release;
+        }
+
+        if (Input.GetButtonDown("A"))
+        {
+            this.ResetTimers();
+            this.CurrentPhase = Phase.Attack;
+            this.InputDirection = -1.0f;
+        }
+
+        if (Input.GetButton("A"))
+        {
+            this.InputDirection = -1.0f;
+        }
+
+        if (Input.GetButtonUp("A"))
+        {
+            this.InputDirection = -1.0f;
+            this.CurrentPhase = Phase.Release;
+        }
+
+        if (this.CurrentPhase != Phase.None)
+        {
+            var position = this.gameObject.transform.position;
+            position.x += this.InputDirection * this.Speed * this.ADSREnvelope() * Time.deltaTime;
+            //Debug.Log(this.InputDirection * this.Speed * this.ADSREnvelope() * Time.deltaTime);
+            Debug.Log(this.CurrentPhase);
+            this.gameObject.transform.position = position;
+        }
+
+        
+
+        if (Input.GetAxis("Vertical") > 0.01 /*&& IsGrounded()*/)
         {
             if (rigidBody != null)
             {
+                Debug.Log("Jumping");
                 rigidBody.velocity = new Vector2(rigidBody.velocity.x, 3.0f);
             }
         }
 
     }
 
-    public void ExecuteLeft(GameObject gameObject)
+    bool IsGrounded()
     {
-        var rigidBody = gameObject.GetComponent<Rigidbody2D>();
-        if (rigidBody != null)
-        {
-            rigidBody.velocity = new Vector2(-this.Speed, rigidBody.velocity.y);
-            gameObject.GetComponent<SpriteRenderer>().flipX = true;
-        }
-    }
-
-    public void ExecuteRight(GameObject gameObject)
-    {
-        var rigidBody = gameObject.GetComponent<Rigidbody2D>();
-        if (rigidBody != null)
-        {
-            rigidBody.velocity = new Vector2(this.Speed, rigidBody.velocity.y);
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
-        }
+        var grounded = Physics.Raycast(this.transform.position, -Vector3.up, this.DistanceToGround + 0.1f);
+        var debugRay = new Vector3(0f, this.transform.position.y - this.DistanceToGround - 0.1f, 0f);
+        Debug.DrawRay(this.transform.position, debugRay, Color.green, 1.0f);
+        Debug.Log(grounded);
+        return grounded;
     }
 }
