a5c6b6be0d69b6371cb27f10d92b9d869b47dc94
gbrusman <gabrielbrusman@gmail.com>
2019-12-04 22:22:22 -0800

    GrappleProjectile Prefab now doesn't have gravity. Prefab now shoots out in the right direction.

--- Assets/Scripts/GrappleProjectileController.cs
+++ Assets/Scripts/GrappleProjectileController.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class GrappleProjectileController : MonoBehaviour
 {
     // Start is called before the first frame update
+    private float Thrust = 100.0f;
     void Start()
     {
-        
+        this.GetComponent<Rigidbody2D>().AddForce(-transform.up * Thrust);
     }
 
     // Update is called once per frame
