bb4768990e4a40406f7300d232fa2a82878df767
nugalm <jmnugal@ucdavis.edu>
2019-12-09 08:07:24 -0800

    player now respawns when hitting a spike in pull stage

--- Assets/Scripts/DeathInPullStage.cs
+++ Assets/Scripts/DeathInPullStage.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,12 +9,16 @@ public class DeathInPullStage : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        player.transform.position = new Vector3(-45.5f, 2.7f, 2.82f);
+        
+        this.player.transform.position = new Vector3(-45.5f, 2.7f, 2.82f);
     }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        this.player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
