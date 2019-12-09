4829577937ceb16d14539e8b74581d10869bf57a
nugalm <jmnugal@ucdavis.edu>
2019-12-09 07:51:33 -0800

    added new Scene For Pull

---
+++ Assets/Scripts/DeathInPullStage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathInPullStage : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.transform.position = new Vector3(-45.5f, 2.7f, 2.82f);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
