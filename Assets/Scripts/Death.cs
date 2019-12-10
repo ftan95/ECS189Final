4a02564c673f43979d363e7252bc7d2615625acd
Frederick <Frederick@Pein-2.local>
2019-12-10 01:03:05 -0800

    Clean up stages, updated SampleScene as tutorial

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -6,15 +6,39 @@ public class Death : MonoBehaviour
 {
     [SerializeField] GameObject player;
     private Vector3 InitialPosition;
+    private List<Vector3> BlockList = new List<Vector3>();
+    private Vector3 BlockPosition;
+    private Vector3 BlockPosition2;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        player.transform.position = this.InitialPosition;
+        
+        if (collision.gameObject.name == "Player")
+        {
+            player.transform.position = this.InitialPosition;
+            //for (int i=0; i < 11; i++)
+            //{
+            //    var position = GameObject.Find("Bridge " + i);
+            //    position.transform.position = this.BlockList[i];
+            //}
+
+        }
+        
+        
+        
     }
     // Start is called before the first frame update
     void Start()
     {
+
         this.InitialPosition = player.transform.position;
+        for (int i = 0; i < 11; i++)
+        {
+            var test = GameObject.Find("Bridge " + i);
+            Debug.Log("Block position " + test.transform);
+            //this.BlockPosition = test.transform.position;
+            //this.BlockList.Add(this.BlockPosition);
+        }
     }
 
     // Update is called once per frame
