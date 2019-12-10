9453a2da45e25a2639d472d306888bc93eaa26a7
Michael Cordero <magcordero@ucdavis.edu>
2019-12-10 04:18:25 -0800

    Created an extremem stage. Implemented moving platform scripts

---
+++ Assets/Scripts/PlatformMovementHorizontal.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovementHorizontal : MonoBehaviour
+{
+    [SerializeField] private float Speed = 10.0f;
+    [SerializeField] private float RightBound;
+    [SerializeField] private float LeftBound;
+    private float BasePosition;
+    // Platform moves -> when Direction is true and <- when Direction is false
+    [SerializeField] private bool Direction;
+    void Start()
+    {
+        BasePosition = this.gameObject.transform.position.x;
+        RightBound = BasePosition + RightBound;
+        LeftBound = BasePosition - LeftBound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var position = this.gameObject.transform.position;
+        if (Direction == true)
+        {
+            position.x += Speed * Time.deltaTime;
+
+            if (position.x > RightBound)
+            {
+                Direction = false;
+            }
+        }
+        else
+        {
+            position.x -= Speed * Time.deltaTime;
+
+            if (position.x < LeftBound)
+            {
+                Direction = true;
+            }
+        }
+
+        this.gameObject.transform.position = position;
+    }
+}
