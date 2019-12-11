58722f8ff4bd8aaf5080998ab1ac23fe71e6ac60
Frederick <Frederick@Pein-2.local>
2019-12-10 18:55:45 -0800

    Fixed block triggering death

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -10,12 +10,10 @@ public class Death : MonoBehaviour
     private Vector3 BlockPosition;
 
     private void OnTriggerEnter2D(Collider2D collision)
-    {
-        
+    {  
         if (collision.gameObject.name == "Player")
         {
             player.transform.position = this.InitialPosition;
-            // GameObject.Find("Bridge 1").transform.position = this.BlockPosition;
             for (int i = 1; i < 11; i++)
             {
                 if (GameObject.Find("Bridge " + i) != null)
@@ -27,26 +25,17 @@ public class Death : MonoBehaviour
             player.GetComponent<DistanceJoint2D>().enabled = false;
             player.GetComponent<GrapplingHook>().Line.enabled = false;
 
-            //// Enable aiming arrow
-            //AimingArrow.GetComponent<SpriteRenderer>().enabled = true;
-
             // Reset variables
             player.GetComponent<GrapplingHook>().GrappleIsActive = false;
             player.GetComponent<GrapplingHook>().IsFirstConnectedFrame = true;
             player.GetComponent<GrapplingHook>().ConnectionIsActive = false;
         }
-        
-        
-        
-        player.transform.position = this.InitialPosition;
         FindObjectOfType<AudioManager>().Play("GameOver");
     }
     // Start is called before the first frame update
     void Start()
     {
-
         this.InitialPosition = player.transform.position;
-       // this.BlockPosition = GameObject.Find("Bridge 1").transform.position;
         for (int i = 1; i < 11; i++)
         {
             var index = "Bridge " + i;
