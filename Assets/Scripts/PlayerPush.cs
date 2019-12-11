5c5d0493dc686aea70a2c08db2aed78c5f44a881
Frederick <Frederick@Pein-2.local>
2019-12-10 22:04:48 -0800

    Fixed infinite jump

--- Assets/Scripts/PlayerPush.cs
+++ Assets/Scripts/PlayerPush.cs
@@ -18,14 +18,14 @@ public class PlayerPush : MonoBehaviour
         Physics2D.queriesStartInColliders = false;
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, this.Distance, this.BoxMask);
 
-        if (hit.collider != null && hit.collider.gameObject.tag == "Pushable" && Input.GetKey(KeyCode.E))
+        if (hit.collider != null && hit.collider.gameObject.tag == "Pushable" && Input.GetKey(KeyCode.U))
         {
             this.Box = hit.collider.gameObject;
             this.Box.GetComponent<FixedJoint2D>().enabled = true;
             this.Box.GetComponent<BoxPull>().BeingPushed = true;
             this.Box.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
         }
-        else if (Input.GetKeyUp(KeyCode.E))
+        else if (Input.GetKeyUp(KeyCode.U))
         {
             this.Box.GetComponent<FixedJoint2D>().enabled = false;
             this.Box.GetComponent<BoxPull>().BeingPushed = false;
